74bf01135d74ffa0fba1624196f023f17a263155
ChelFannie <912552857@qq.com>
2019-01-09 17:54:30 +0800

    修改进度条样式

--- src/page/my-meetings.vue
+++ src/page/my-meetings.vue
@@ -7,7 +7,7 @@
           <div class="tab-item">
             <meeting-info @go-detail="goDetail">
               <div v-if="index===1" slot="container">
-                <van-steps :active="activeStep" active-color="#3FB6FA">
+                <van-steps :active="activeStep">
                   <van-step v-for="(step, index) in activeStepList" :key="index">{{step}}</van-step>
                 </van-steps>
               </div>
@@ -124,48 +124,48 @@ export default {
 
     /* 进度样式 */
     .van-steps--horizontal{
-      padding: 30px 30px 0;
+      padding: 0 30px 30px;
+      .van-steps__items.van-steps__items--alone{
+        padding-top: 0;
+      }
       .van-steps__items{
-        padding-top: 40px;
+        padding-bottom: 60px;
         margin: 0;
       }
-      .van-step{
-        .van-step__circle-container{
-          top: -30px;
-          .van-icon{
-            font-size: 20px;
-          }
-        }
+      .van-step--horizontal:last-child .van-step__circle-container{
+        right: -11px;
+      }
+      .van-step__circle{
+        width: 10px;
+        height: 10px;
+      }
+      .van-step__title{
+        font-size: 24px;
+        position: relative;
+        top: 55px;
+      }
+      .van-step__line{
+        height: 6px;
+        background-color: rgba(153,153,153);
+      }
+      .van-step--finish{
+        color: #3FB6FA;
+        .van-step__circle,
         .van-step__line{
-          background-color: rgba(153,153,153);
-          top: -28px;
-          height: 6px;
-        }
-        .van-step__title{
-          font-size: 24px;
-        }
-        &.van-step--finish {
-          color: #3FB6FA;
-          .van-step__circle,
-          .van-step__line {
-            background-color: #3FB6FA;
-          }
-        }
-        .van-step__circle {
-          width: 10px;
-          height: 10px;
-          border-radius: 50%;
-        }
-        &.van-step--process {
-          .van-icon-checked {
-            top: -5px;
-            font-size: 20px;
-          }
+          background-color: #3FB6FA;
         }
       }
-    }
-    .van-step--horizontal:last-child .van-step__circle-container{
-      right: -12px;
+      .van-step .van-icon{
+        font-size: 20px;
+      }
+      .van-step--process{
+        .van-step__title{
+          color: #3FB6FA!important;
+        }
+        .van-icon-checked{
+          color: #3FB6FA!important;
+        }
+      }
     }
     /* 进度样式 */
   }
